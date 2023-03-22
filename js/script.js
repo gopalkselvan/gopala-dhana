28bf03accd5f783baf79e008006947198766e10f
Gopala Karthigai Selvan Murugesan <48343790+gopalkselvan@users.noreply.github.com>
2023-03-22 10:21:46 +0530

    AllChanges

---
+++ js/script.js
@@ -0,0 +1,62 @@
+/**
+ * @author Gopala Murugesan
+ */
+(function($) {
+	"use strict";
+	$('.sakura-falling').sakura();
+
+	$(document).ready(function() {
+
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > $(window).height()) {
+				$('.navbar').addClass('fixed');
+				$('body').css('padding-top', '97px');
+			} else {
+				$('.navbar').removeClass('fixed');
+				$('body').css('padding-top', '0');
+			}
+		});
+
+	})
+
+})(jQuery);
+
+$(document).on('click', function() {
+	document.getElementById("my_audio").paused ? document.getElementById("my_audio").play() : document.getElementById("my_audio").pause();
+});
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jun 9, 2023 06:30:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+	// Get todays date and time
+	var now = new Date().getTime();
+
+	// Find the distance between now and the count down date
+	var distance = countDownDate - now;
+
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	// Output the result in an element with id="timer"
+	document.getElementById("timer").innerHTML = "<div class=\"days\"> \
+    <div class=\"numbers\">" + days + "</div>days</div> \
+  <div class=\"hours\"> \
+    <div class=\"numbers\">" + hours + "</div>hours</div> \
+  <div class=\"minutes\"> \
+    <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+  <div class=\"seconds\"> \
+    <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+  </div>";
+
+	// If the count down is over, write some text 
+	if (distance < 0) {
+		clearInterval(x);
+		document.getElementById("timer").innerHTML = "Please bless the couple with a loving, strong, and long-lasting marriage filled with joy, understanding, and mutual respect.";
+	}
+}, 1000);
